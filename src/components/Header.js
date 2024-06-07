486e703b4ea4105684260e5ab78321c0f2f003fa
Jonathan Marshall <jwmarsh16@gmail.com>
2024-06-07 18:52:18 -0500

    lab conmplete

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Header({ name }) {
+    return (
+      <div className="Header">
+        <header>
+            <h1>{name}</h1>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Header;
